221d1ea9fb148cd82215645d189d8717fbfda366
young <55731744@qq.com>
2025-01-06 15:33:35 +0800

    更新，git忽略文件

--- eslint.config.js
+++ eslint.config.js
@@ -10,7 +10,7 @@ export default [
 
   {
     name: 'app/files-to-ignore',
-    ignores: ['**/dist/**', '**/dist-ssr/**', '**/coverage/**', 'vite.config.ts'],
+    ignores: ['**/dist/**', '**/dist-ssr/**', '**/coverage/**', 'vite.config.ts', '**d.ts'],
   },
 
   ...pluginVue.configs['flat/essential'],
