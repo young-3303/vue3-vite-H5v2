7e38fb72c798f3f2ef66c45154f048ad6f18eb5c
young <55731744@qq.com>
2025-01-13 16:06:35 +0800

    element-plus 测试

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,8 @@ import 'core-js/features/object/has-own.js'
 import './assets/main.css'
 
 import { createApp } from 'vue'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
 import App from './App.vue'
 import router from './router'
 
@@ -9,7 +11,9 @@ const app = createApp(App)
 
 app.use(router)
 
-app.mount('#app')
+app
+  .use(ElementPlus)
+  .mount('#app')
 function Test() {
   return new Promise((resolve) => {
     setTimeout(() => {
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,35 +1,159 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-    <div class="flex-box">
-      <div class="c1"></div>
-      <div class="c2"></div>
-    </div>
+  <el-table
+      ref="multipleTableRef"
+      :data="tableData"
+      row-key="id"
+      style="width: 100%"
+      @selection-change="handleSelectionChange"
+      @expand-change="expandChange"
+  >
+    <el-table-column width="55" type="expand">
+      <template #default="scope">
+        <el-table ref="childRef" @select="childSelect" :data="scope.row.childData">
+          <el-table-column type="selection"></el-table-column>
+          <el-table-column prop="name" label="Name" />
+          <el-table-column prop="age" label="age" />
+        </el-table>
+      </template>
+    </el-table-column>
+    <el-table-column type="selection" :selectable="selectable" width="55" />
+    <el-table-column label="Date" width="120">
+      <template #default="scope">{{ scope.row.date }}</template>
+    </el-table-column>
+    <el-table-column property="name" label="Name" width="120" />
+    <el-table-column property="address" label="Address" />
+  </el-table>
+  <div style="margin-top: 20px">
+    <el-button @click="toggleSelection([tableData[1], tableData[2]])">
+      Toggle selection status of second and third rows
+    </el-button>
+    <el-button @click="toggleSelection([tableData[1], tableData[2]], false)">
+      Toggle selection status based on selectable
+    </el-button>
+    <el-button @click="toggleSelection()">Clear selection</el-button>
   </div>
 </template>
-<script setup>
+
+<script lang="ts" setup>
+import {nextTick, ref} from 'vue'
+import type { TableInstance } from 'element-plus'
+
+interface Child {
+  name: string
+  age: number
+  selected: boolean
+}
+interface User {
+  id: number
+  date: string
+  name: string
+  address: string
+  childData?: Child[]
+}
+
+const multipleTableRef = ref<TableInstance>()
+const multipleSelection = ref<User[]>([])
+const childRef = ref<TableInstance>()
+const selectable = (row: User) => ![1, 2].includes(row.id)
+const toggleSelection = (rows?: User[], ignoreSelectable?: boolean) => {
+  if (rows) {
+    rows.forEach((row) => {
+      multipleTableRef.value!.toggleRowSelection(
+          row,
+          undefined,
+          ignoreSelectable
+      )
+    })
+  } else {
+    multipleTableRef.value!.clearSelection()
+  }
+}
+const handleSelectionChange = (val: User[]) => {
+  multipleSelection.value = val
+}
+
+function childSelect(selection: Child[], row: Child) {
+  row.selected = !(row.selected)
+}
+function expandChange(row: User) {
+  nextTick(() => {
+    row.childData?.forEach((item) => {
+      if (item.selected) {
+        childRef.value?.toggleRowSelection(item)
+      }
+    })
+  })
+}
+const tableData: User[] = [
+  {
+    id: 1,
+    date: '2016-05-03',
+    name: 'Tom',
+    address: 'No. 189, Grove St, Los Angeles',
+    childData: [
+      {name: 'zs', age: 22, selected: false},
+      {name: 'ls', age: 24, selected: false}
+    ]
+  },
+  {
+    id: 2,
+    date: '2016-05-02',
+    name: 'Tom',
+    address: 'No. 189, Grove St, Los Angeles',
+    childData: [
+      {name: 'zs', age: 22, selected: false},
+      {name: 'ls', age: 24, selected: false}
+    ]
+  },
+  {
+    id: 3,
+    date: '2016-05-04',
+    name: 'Tom',
+    address: 'No. 189, Grove St, Los Angeles',
+    childData: [
+      {name: 'zs', age: 22, selected: false},
+      {name: 'ls', age: 24, selected: false}
+    ]
+  },
+  {
+    id: 4,
+    date: '2016-05-01',
+    name: 'Tom',
+    address: 'No. 189, Grove St, Los Angeles',
+    childData: [
+      {name: 'zs', age: 22, selected: false},
+      {name: 'ls', age: 24, selected: false}
+    ]
+  },
+  {
+    id: 5,
+    date: '2016-05-08',
+    name: 'Tom',
+    address: 'No. 189, Grove St, Los Angeles',
+    childData: [
+      {name: 'zs', age: 22, selected: false},
+      {name: 'ls', age: 24, selected: false}
+    ]
+  },
+  {
+    id: 6,
+    date: '2016-05-06',
+    name: 'Tom',
+    address: 'No. 189, Grove St, Los Angeles',
+    childData: [
+      {name: 'zs', age: 22, selected: false},
+      {name: 'ls', age: 24, selected: false}
+    ]
+  },
+  {
+    id: 7,
+    date: '2016-05-07',
+    name: 'Tom',
+    address: 'No. 189, Grove St, Los Angeles',
+    childData: [
+      {name: 'zs', age: 22, selected: false},
+      {name: 'ls', age: 24, selected: false}
+    ]
+  },
+]
 </script>
-<style lang="scss">
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
-}
-.flex-box {
-  display: flex;
-  //gap: 20px;
-  @include gap(20px);
-  .c1 {
-   background: aqua;
-  }
-  .c2 {
-    background: rosybrown;
-  }
-  >div {
-    width: 200px;
-    height: 200px;
-  }
-}
-</style>
